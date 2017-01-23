f047f420caad15f083973a25cf847070b592b9f7
ian <me@iany.me>
2017-01-23 21:46:04 +0800

    disable autocrlf

--- bin/setup.bat
+++ bin/setup.bat
@@ -1,3 +1,5 @@
 if not exist config\roster copy config\roster.example config\roster
 if not exist config\master copy config\master.example config\master
 if not exist config\minion copy config\minion.example config\minion
+
+git config --global core.autocrlf false
