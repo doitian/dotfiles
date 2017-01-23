9cc6ec7cca6cf0d44fdaaaeab27094b50e2a606a
ian <me@iany.me>
2017-01-23 16:50:28 +0800

    Fix for Windows.

---
+++ bin/setup.bat
@@ -0,0 +1,3 @@
+if not exist config\roster copy config\roster.example config\roster
+if not exist config\master copy config\master.example config\master
+if not exist config\minion copy config\minion.example config\minion
