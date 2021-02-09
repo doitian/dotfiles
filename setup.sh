3d81b0551549a4e7c414918740a70a5bb6fedcc1
ian <me@iany.me>
2021-02-09 12:58:50 +0800

    do not clone private repo in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
 
 # for GitHub Codespaces <https://docs.github.com/en/github/developing-online-with-codespaces/personalizing-codespaces-for-your-account>
 
-ssh-keyscan -H github.com | sudo -H -u ian tee .ssh/known_hosts
+mkdir -p "$HOME/.ssh"
+ssh-keyscan -H github.com | tee "$HOME/.ssh/known_hosts"
 ln -snf "$HOME/dotfiles" "$HOME/.dotfiles"
 
 ./debian.sh
-./manage.sh r -p
-./manage.sh i -p
+./manage.sh r
+./manage.sh i
