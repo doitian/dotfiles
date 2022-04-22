6c8d9d189e37a13233e73502102bcc0df829d923
ian <me@iany.me>
2022-04-22 13:58:24 +0800

    setup: only ln .dotfiles when not found

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 
 # for GitHub Codespaces <https://docs.github.com/en/github/developing-online-with-codespaces/personalizing-codespaces-for-your-account>
 
-ln -snf "$HOME/dotfiles" "$HOME/.dotfiles"
+if [ -d "$HOME/dotfiles" ] && ! [ -d "$HOME/.dotfiles" ]; then
+  ln -snf "$HOME/dotfiles" "$HOME/.dotfiles"
+fi
 cd "$HOME/.dotfiles"
 mkdir -p "$HOME/.ssh"
 ssh-keyscan -H github.com | tee "$HOME/.ssh/known_hosts"
