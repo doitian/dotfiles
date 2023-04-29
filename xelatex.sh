31bf191c67276e120860f9546ef2da90f7ba4977
ian <me@iany.me>
2023-04-29 20:34:17 +0800

    install xelatex for Ubuntu

--- xelatex.sh
+++ xelatex.sh
@@ -5,22 +5,33 @@ set -u
 set -x
 
 UNAME="$(uname -s)"
+TLPKGS=(
+  texliveonfly xelatex adjustbox tcolorbox collectbox ucs environ
+  trimspaces titling enumitem rsfs xecjk fvextra svg transparent
+)
 
 case "$UNAME" in
   Darwin)
     brew install librsvg
     brew install --cask basictex
     eval "$(/usr/libexec/path_helper)"
+
+    sudo tlmgr update --self
+    sudo tlmgr update --all
+    for pkg in "${TLPKGS[@]}"; do
+      sudo tlmgr install $pkg
+    done
+    ;;
+  Linux)
+    if type -f apt &>/dev/null; then
+      sudo apt install texlive-xetex
+    else
+      echo "Unsupported system $UNAME" >&2
+      exit 1
+    fi
     ;;
   *)
     echo "Unsupported system $UNAME" >&2
     exit 1
     ;;
 esac
-
-sudo tlmgr update --self
-sudo tlmgr update --all
-for pkg in texliveonfly xelatex adjustbox tcolorbox collectbox ucs environ \
-  trimspaces titling enumitem rsfs xecjk fvextra svg transparent; do
-  sudo tlmgr install $pkg
-done
