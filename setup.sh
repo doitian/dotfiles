1899584cf81a67dea3f923579b96560471dba130
ian <me@iany.me>
2024-03-20 00:01:52 +0000

    fix setup.sh for gitpod

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$DOTFILES_DIR"
 
 mkdir -p "$HOME/.ssh"
 ssh-keyscan -H github.com | tee "$HOME/.ssh/known_hosts"
@@ -14,15 +15,43 @@ if [ -n "${GITPOD_WORKSPACE_ID:-}" ]; then
 fi
 
 if command -v brew &>/dev/null; then
-  ./debian.sh --apt --brew
+  if [ -d "$DOTFILES_DIR/repos/private" ]; then
+    ./manage.sh r -p
+    ./manage.sh i -p
+    (
+      ./debian.sh --apt --brew
+      ./manage.sh r -p
+      ./manage.sh i -p
+    ) &
+    disown
+  else
+    ./manage.sh r
+    ./manage.sh i
+    (
+      ./debian.sh --apt --brew
+      ./manage.sh r
+      ./manage.sh i
+    ) &
+    disown
+  fi
 else
-  ./debian.sh --apt
-fi
-
-if [ -d "$DOTFILES_DIR/repos/private" ]; then
-  ./manage.sh r -p
-  ./manage.sh i -p
-else
-  ./manage.sh r
-  ./manage.sh i
+  if [ -d "$DOTFILES_DIR/repos/private" ]; then
+    ./manage.sh r -p
+    ./manage.sh i -p
+    (
+      ./debian.sh --apt
+      ./manage.sh r -p
+      ./manage.sh i -p
+    ) &
+    disown
+  else
+    ./manage.sh r
+    ./manage.sh i
+    (
+      ./debian.sh --apt
+      ./manage.sh r
+      ./manage.sh i
+    ) &
+    disown
+  fi
 fi
